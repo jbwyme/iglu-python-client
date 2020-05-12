b96dc3309a6907d87db215a19a388b272b496bb9
Josh Wymer <wymetyme@gmail.com>
2020-05-12 09:33:43 -0500

    prepare v0.2.0 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iglu-client",
-    version="0.1.1",
+    version="0.2.0",
     author="Josh Wymer",
     author_email="josh@mixpanel.com",
     description="A python client for retrieving Iglu schemas and validating self-describing JSON",
